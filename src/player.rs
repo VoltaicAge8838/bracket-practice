706c42b09f249b7a9f519ceec105236be5ec1b26
VoltaicAge8838 <brocam@gmail.com>
2022-01-18 20:40:18 -0700

    basic monsters

--- src/player.rs
+++ src/player.rs
@@ -1,7 +1,7 @@
-use rltk::{Rltk, VirtualKeyCode};
+use rltk::{Rltk, VirtualKeyCode, Point };
 use specs::prelude::*;
 use std::cmp::{max, min};
-use super::{Position, Player, TileType, Map, State, Viewshed};
+use super::{Position, Player, TileType, Map, State, Viewshed, RunState };
 
 
 pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
@@ -16,14 +16,18 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
             pos.x = min(79, max( 0, pos.x + delta_x));
             pos.y = min(49, max( 0, pos.y + delta_y));
 
+            let mut ppos = ecs.write_resource::<Point>();
+            ppos.x = pos.x;
+            ppos.y = pos.y;
+
             viewshed.dirty = true;
         }
     }
 }
 
-pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
+pub fn player_input(gs: &mut State, ctx: &mut Rltk) -> RunState {
     match ctx.key {
-        None => {}
+        None => { return RunState::Paused }
         Some(key) => match key {
             VirtualKeyCode::Left |
             VirtualKeyCode::Numpad4 |
@@ -41,7 +45,8 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
             VirtualKeyCode::Numpad2 |
             VirtualKeyCode::J => try_move_player(0, 1, &mut gs.ecs),
 
-            _ => {}
+            _ => { return RunState::Paused }
         }
     }
+    RunState::Running
 }
